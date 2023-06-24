a5fa32c8f5c460998bf90b51ab968f1b1ebe45c7
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-06-24 16:36:24 +0200

    Added example dispatch with variable value, changed exampleStore type definition and reset state  to better match official documentation and practices

--- src/components/exampleCount.tsx
+++ src/components/exampleCount.tsx
@@ -1,12 +1,14 @@
 import { useExampleStore } from "~/store";
 
 export default function ExampleCount() {
-  const { count, increment, decrement, reset } = useExampleStore();
+  const { count, increase, increment, decrement, reset } = useExampleStore();
+  const value: number = 10;
 
   return (
     <div className="rounded bg-zinc-100 p-2 ">
       <p className="text-center">Count is {count}</p>
       <div className="flex gap-2">
+        <Button onClick={() => increase(value)}>Increase by {value}</Button>
         <Button onClick={increment}>Increase</Button>
         <Button onClick={decrement}>Decrease</Button>
         <Button onClick={reset}>Reset</Button>
--- src/store/exampleStore.ts
+++ src/store/exampleStore.ts
@@ -1,18 +1,27 @@
 import { create } from "zustand";
 import { createSelectors } from "./selectors";
 
-interface ExampleState {
+interface State {
   count: number;
+}
+
+interface Actions {
+  increase: (amount: number) => void;
   increment: () => void;
   decrement: () => void;
   reset: () => void;
 }
 
-const useExampleStore = create<ExampleState>((set) => ({
+const initialState: State = {
   count: 0,
+};
+
+const useExampleStore = create<State & Actions>((set) => ({
+  ...initialState,
+  increase: (amount) => set((state) => ({ count: state.count + amount })),
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
-  reset: () => set({ count: 0 }),
+  reset: () => set(initialState),
 }));
 
 export default createSelectors(useExampleStore);
