f7407fdc6ab3956349a6640cc48bd377920e3c0b
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-06-24 10:11:54 +0200

    Removed example query and updated import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import AuthButton from "~/components/ui/AuthButton";
+import { AuthButton } from "~/components/ui";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -16,9 +14,6 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
             <AuthShowcase />
           </div>
         </div>
