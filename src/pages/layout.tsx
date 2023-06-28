85dacd935cd08323655dc3fe05ee46994f8c8062
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-06-28 16:03:07 +0200

    update shadcn default button with brand styling

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
-import { fontSans } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
+import { fontSans } from "~/lib/fonts";
+import { cn } from "~/lib/utils";
 
 type LayoutProps = {
   children: ReactNode;
